eec782e1d2f9051b947f23ce386abaa709727c4a
clock-head <james.n.kai92@gmail.com>
2023-10-03 17:06:14 +1000

    update secret

--- frontend/container/config/webpack.prod.js
+++ frontend/container/config/webpack.prod.js
@@ -17,7 +17,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        products: `products@${domain}/www.liquidfire.tech/products/latest/remoteEntry.js`,
+        products: `products@${domain}/products/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
